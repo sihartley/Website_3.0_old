635c72fd25c0b5d6b5e5a364b5b9af53f63068b3
Simon Hartley <sihartley@msn.com>
2022-05-19 18:01:18 -0700

    Initial Site Creation with Grunt

---
+++ Public/index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vinyl Imagination v3.0 Responsive, Mobile First Website</title>
+  </head>
+  <body>
+    <div id="page">
+      <header></header>
+
+      <section class=""></section>
+
+      <section class=""></section>
+
+      <section class=""></section>
+
+      <nav></nav>
+
+      <footer></footer>
+    </div>
+
+    <script src="http://localhost:35729/livereload.js"></script>
+  </body>
+</html>
+
